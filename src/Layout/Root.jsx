45e8cfb4f6b6b6a71d6895b1f707f41186a763dc
abunaim5 <abunaim140@gmail.com>
2024-03-25 22:58:05 +0600

    created react router dom file and added nav component

---
+++ src/Layout/Root.jsx
@@ -0,0 +1,11 @@
+import Header from "../components/Header/Header";
+
+const Root = () => {
+    return (
+        <div className="max-w-[1440px] mx-auto pt-12">
+            <Header></Header>
+        </div>
+    );
+};
+
+export default Root;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,39 @@
+import { Link, NavLink } from "react-router-dom";
+
+const Header = () => {
+
+    const links = <>
+        <NavLink><li><a>Home</a></li></NavLink>
+        <NavLink><li><a>Listed Books</a></li></NavLink>
+        <NavLink><li><a>Pages to Read</a></li></NavLink>
+        <NavLink><li><a>Premium</a></li></NavLink>
+        <NavLink><li><a>Cart</a></li></NavLink>
+    </>
+
+    return (
+        <div className="navbar bg-base-100">
+            <div className="navbar-start">
+                <div className="dropdown">
+                    <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                    </div>
+                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52 text-lg font-work text-[#131313CC]">
+                        {links}
+                    </ul>
+                </div>
+                <Link><button className="btn btn-ghost text-3xl font-bold font-work gap-0 rounded-lg"><span className="text-[#23BE0A]">Fiction</span>Finder</button></Link>
+            </div>
+            <div className="navbar-center hidden lg:flex">
+                <ul className="menu menu-horizontal px-1 text-lg font-work text-[#131313CC]">
+                    {links}
+                </ul>
+            </div>
+            <div className="navbar-end text-lg font-work font-semibold space-x-4">
+                <button className="btn text-white hover:text-[#131313] bg-[#23BE0A] hover:bg-transparent border-2 border-transparent hover:border-[#23BE0A] rounded-lg">Sign In</button>
+                <button className="btn text-white hover:text-[#131313] bg-[#59C6D2] hover:bg-transparent border-2 border-transparent hover:border-[#59C6D2] rounded-lg">Sign Up</button>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
