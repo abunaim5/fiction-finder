52be6bb5bf21ce8e3f261c9a880562faad01b724
abunaim5 <abunaim140@gmail.com>
2024-03-26 00:36:05 +0600

    added books and book card component

---
+++ src/components/BookCard/BookCard.jsx
@@ -0,0 +1,28 @@
+import image from '/images/bannerImg.png'
+
+const BookCard = () => {
+    return (
+        <div className='mt-9 p-6 rounded-2xl border border-[#13131326]'>
+            <div className='flex items-center justify-center px-24 py-8 bg-[#F3F3F3] rounded-2xl'>
+                <img src={image} alt="" />
+            </div>
+            <div className='flex gap-3 mt-6'>
+                <button className='btn w-auto h-auto px-4 py-2 rounded-full border-0 text-base bg-[#23BE0A0D] font-work font-medium text-[#23BE0A]'>Young Adult</button>
+                <button className='btn w-auto h-auto px-4 py-2 rounded-full border-0 text-base bg-[#23BE0A0D] font-work font-medium text-[#23BE0A]'>Identity</button>
+            </div>
+            <div className='mt-4 divide-y-2 divide-dashed space-y-5'>
+                <div>
+                    <h2 className='text-2xl font-bold text-[#131313] mb-4'>The Catcher in the Rye</h2>
+                    <h4 className='font-work font-medium text-[#131313CC]'>By : Awlad Hossain</h4>
+                </div>
+                <div></div>
+            </div>
+            <div className='flex items-center justify-between font-work font-medium text-[#131313CC] mt-4'>
+                <h4 className=''>Fiction</h4>
+                <h4><span>5.0</span> <span>icon</span></h4>
+            </div>
+        </div>
+    );
+};
+
+export default BookCard;
---
+++ src/components/Books/Books.jsx
@@ -0,0 +1,12 @@
+import BookCard from "../BookCard/BookCard";
+
+const Books = () => {
+    return (
+        <div className="mt-24">
+            <h2 className="text-[40px] text-[#131313] font-bold text-center">Books</h2>
+            <BookCard></BookCard>
+        </div>
+    );
+};
+
+export default Books;
