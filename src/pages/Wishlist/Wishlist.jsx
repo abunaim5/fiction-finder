04b5a8d4336a2213436202f584ae7189c5f8ac51
abunaim5 <abunaim140@gmail.com>
2024-03-27 02:35:25 +0600

    added data in local storage by clicking wishlist and load data in local stroge and display the data in wishlist page

--- src/pages/Wishlist/Wishlist.jsx
+++ src/pages/Wishlist/Wishlist.jsx
@@ -1,9 +1,22 @@
 import CategoryListCard from "../../components/CategoryListCard/CategoryListCard";
+import { useLoaderData } from "react-router-dom";
+import { handleGetBooksIdInLocal } from "../../utils/localBooks/localBooks";
+
+const wishlistIdList = handleGetBooksIdInLocal('wishlist');
 
 const Wishlist = () => {
+    const books = useLoaderData();
+
     return (
-        <div>
-            <CategoryListCard></CategoryListCard>
+        <div className="mt-8 mb-40 space-y-6">
+            {
+                books.map(book => {
+                    const findBookInLocal = wishlistIdList.find(id => id === book.bookId);
+                    if (findBookInLocal) {
+                        return <CategoryListCard key={book.bookId} book={book} ></CategoryListCard>
+                    }
+                })
+            }
         </div>
     );
 };
